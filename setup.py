586757f0d8f767bff60fb6497f76865c99013c7d
Cong Feng <cong.feng1@utdallas.edu>
2019-03-04 11:12:55 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='OpenSolar-Python',
+      version='0.1',
+      description='Promoting the Openness and Accessibility of Diverse Public Solar Datasets',
+      url='https://github.com/username/repo',
+      author='Cong FENG',
+      author_email='cxf151330@utdallas.edu',
+      license='MIT',
+      packages=['module'],
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5'])
