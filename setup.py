58554a0b43b261d0d74c61ab63fb038bffa93d09
Cong Feng <cong.feng1@utdallas.edu>
2019-03-04 11:14:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import setup
 setup(name='OpenSolar-Python',
       version='0.1',
       description='Promoting the Openness and Accessibility of Diverse Public Solar Datasets',
-      url='https://github.com/username/repo',
+      url='https://github.com/fengcong1992/OpenSolar_Python',
       author='Cong FENG',
       author_email='cxf151330@utdallas.edu',
       license='MIT',
-      packages=['module'],
-      install_requires=['numpy>=1.11',
-                        'matplotlib>=1.5'])
+      packages=setuptools.find_packages())
